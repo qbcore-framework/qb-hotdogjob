9d4eb4cdb19a58949b67dd663701e0b11db07e19
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2023-02-15 20:23:55 +0100

    Update pt.lua
    Added missing translations

--- locales/pt.lua
+++ locales/pt.lua
@@ -7,6 +7,7 @@ local Translations = {
         no_stand_found = 'Não devolvemos o teu depósito, porque não entregaste o teu stand!',
         no_more = 'Não consegues carregar mais stock de %{value} no teu rank',
         deposit_notreturned = 'Não tens um stand de cachorros quentes',
+        no_dogs = 'Não tens cachorros quentes',
     },
     success = {
         deposit = 'Pagaste um depósito de %{deposit}€ pelo stand!',
@@ -25,13 +26,19 @@ local Translations = {
         grab_stall = '[~g~G~s~] Empurrar Stand',
         drop_stall = '[~g~G~s~] Soltar Stand',
         grab = 'Soltar Stand',
+        prepare = 'Preparar cachorro-quente',
+        toggle_sell = 'Ativar venda',
         selling_prep = '[~g~E~s~] A preparar cachorro [Vendas: ~g~Abertas~w~]',
         not_selling = '[~g~E~s~] A preparar cachorro [Vendas: ~r~Fechadas~w~]',
         sell_dogs = '[~g~7~s~] Vender %{value} x Cachorros por %{value2}€ / [~g~8~s~] Rejeitar',
+        sell_dogs_target = 'Vender %{value} x Cachorros for $%{value2}€',
         admin_removed = "Stand De Cachorros Quentes Removido",
         label_a = "Perfeito (A)",
         label_b = "Raro (B)",
         label_c = "Comum (C)"
+    },
+    keymapping = {
+        gkey = 'Apagar Stand de Cachorro Quentes',
     }
 }
 
