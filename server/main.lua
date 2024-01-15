76e1e94d0254e6db0a9df7cf48fbdd44fae154df
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-01-15 00:32:39 -0600

    Money reasons

--- server/main.lua
+++ server/main.lua
@@ -7,7 +7,7 @@ QBCore.Functions.CreateCallback('qb-hotdogjob:server:HasMoney', function(source,
     local Player = QBCore.Functions.GetPlayer(source)
 
     if Player.PlayerData.money.bank >= Config.StandDeposit then
-        Player.Functions.RemoveMoney('bank', Config.StandDeposit)
+        Player.Functions.RemoveMoney('bank', Config.StandDeposit, 'hot dog deposit')
         Bail[Player.PlayerData.citizenid] = true
         cb(true)
     else
@@ -20,7 +20,7 @@ QBCore.Functions.CreateCallback('qb-hotdogjob:server:BringBack', function(source
     local Player = QBCore.Functions.GetPlayer(source)
 
     if Bail[Player.PlayerData.citizenid] then
-        Player.Functions.AddMoney('bank', Config.StandDeposit)
+        Player.Functions.AddMoney('bank', Config.StandDeposit, 'hot dog deposit')
         cb(true)
     else
         cb(false)
@@ -35,57 +35,57 @@ RegisterNetEvent('qb-hotdogjob:server:Sell', function(coords, amount, price)
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
     if #(pCoords - coords) > 4 then exports['qb-core']:ExploitBan(src, 'hotdog job') end
-    Player.Functions.AddMoney('cash', tonumber(amount * price), 'hotdog')
+    Player.Functions.AddMoney('cash', tonumber(amount * price), 'sold hotdog')
 end)
 
 RegisterNetEvent('qb-hotdogjob:server:UpdateReputation', function(quality)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local JobReputation = Player.PlayerData.metadata["jobrep"]
+    local JobReputation = Player.PlayerData.metadata['jobrep']
 
-    if quality == "exotic" then
-        if JobReputation["hotdog"] ~= nil and JobReputation["hotdog"] + 3 > Config.MaxReputation then
-            JobReputation["hotdog"] = Config.MaxReputation
-            Player.Functions.SetMetaData("jobrep", JobReputation)
+    if quality == 'exotic' then
+        if JobReputation['hotdog'] ~= nil and JobReputation['hotdog'] + 3 > Config.MaxReputation then
+            JobReputation['hotdog'] = Config.MaxReputation
+            Player.Functions.SetMetaData('jobrep', JobReputation)
             TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
             return
         end
-        if JobReputation["hotdog"] == nil then
-            JobReputation["hotdog"] = 3
+        if JobReputation['hotdog'] == nil then
+            JobReputation['hotdog'] = 3
         else
-            JobReputation["hotdog"] = JobReputation["hotdog"] + 3
+            JobReputation['hotdog'] = JobReputation['hotdog'] + 3
         end
-    elseif quality == "rare" then
-        if JobReputation["hotdog"] ~= nil and JobReputation["hotdog"] + 2 > Config.MaxReputation then
-            JobReputation["hotdog"] = Config.MaxReputation
-            Player.Functions.SetMetaData("jobrep", JobReputation)
+    elseif quality == 'rare' then
+        if JobReputation['hotdog'] ~= nil and JobReputation['hotdog'] + 2 > Config.MaxReputation then
+            JobReputation['hotdog'] = Config.MaxReputation
+            Player.Functions.SetMetaData('jobrep', JobReputation)
             TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
             return
         end
-        if JobReputation["hotdog"] == nil then
-            JobReputation["hotdog"] = 2
+        if JobReputation['hotdog'] == nil then
+            JobReputation['hotdog'] = 2
         else
-            JobReputation["hotdog"] = JobReputation["hotdog"] + 2
+            JobReputation['hotdog'] = JobReputation['hotdog'] + 2
         end
-    elseif quality == "common" then
-        if JobReputation["hotdog"] ~= nil and JobReputation["hotdog"] + 1 > Config.MaxReputation then
-            JobReputation["hotdog"] = Config.MaxReputation
-            Player.Functions.SetMetaData("jobrep", JobReputation)
+    elseif quality == 'common' then
+        if JobReputation['hotdog'] ~= nil and JobReputation['hotdog'] + 1 > Config.MaxReputation then
+            JobReputation['hotdog'] = Config.MaxReputation
+            Player.Functions.SetMetaData('jobrep', JobReputation)
             TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
             return
         end
-        if JobReputation["hotdog"] == nil then
-            JobReputation["hotdog"] = 1
+        if JobReputation['hotdog'] == nil then
+            JobReputation['hotdog'] = 1
         else
-            JobReputation["hotdog"] = JobReputation["hotdog"] + 1
+            JobReputation['hotdog'] = JobReputation['hotdog'] + 1
         end
     end
-    Player.Functions.SetMetaData("jobrep", JobReputation)
+    Player.Functions.SetMetaData('jobrep', JobReputation)
     TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
 end)
 
 -- Commands
 
-QBCore.Commands.Add("removestand", Lang:t("info.command"), {}, false, function(source, _)
+QBCore.Commands.Add('removestand', Lang:t('info.command'), {}, false, function(source, _)
     TriggerClientEvent('qb-hotdogjob:staff:DeletStand', source)
 end, 'admin')
